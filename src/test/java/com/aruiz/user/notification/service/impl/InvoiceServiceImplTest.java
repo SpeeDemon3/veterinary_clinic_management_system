985b7177171715f772cd71571b7913fbb0ac861b
Antonio Ruiz Benito <3speedemon3@gmail.com>
2024-08-03 08:45:14 +0200

    Implemented owner service test

---
+++ src/test/java/com/aruiz/user/notification/service/impl/InvoiceServiceImplTest.java
@@ -0,0 +1,49 @@
+package com.aruiz.user.notification.service.impl;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+
+@ExtendWith(MockitoExtension.class)
+class InvoiceServiceImplTest {
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findByClientDni() {
+    }
+
+    @Test
+    void findByState() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void updateById() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void invoicesInfoDownloadCsv() {
+    }
+
+    @Test
+    void invoicesInfoDownloadJson() {
+    }
+}
